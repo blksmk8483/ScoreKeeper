dce464fd481db2d7e571c264281b77aaa8aa9f6f
Brandon Knight <knightbrandon1984@gmail.com>
2022-12-26 23:50:47 -0600

    added a README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ScoreKeeper
-A Score Keeper app using HTML and JavaScript with Bulma as the CSS framework
+A simple Score Keeper app using HTML and JavaScript with Bulma as the CSS framework. 
+
+![Score Keeper](Assets/Pic1.png)
+
+[Click here](https://blksmk8483.github.io/ScoreKeeper/) to try it yourself!
+
+## Description 
+This was practice for me using more javascript and I also wanted to try the basics of a different CSS framework. I chose to try Bulma.
+
+## Future Development
+- I will add the option of having more than 2 players. 
+- Possibly adding on to this so I can save my scores for future play.
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+---
+
--- app.js
+++ app.js
@@ -11,32 +11,32 @@ let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-p1Button.addEventListener('click', function() {
-    if(!isGameOver){
+p1Button.addEventListener('click', function () {
+    if (!isGameOver) {
         p1Score += 1;
-    if (p1Score === winningScore) {
-        isGameOver = true;
-        p1Display.classList.add('has-text-success');
-        p2Display.classList.add('has-text-danger');
-        p1Button.disabled = true;
-        p2Button.disabled = true;
-    }
-    p1Display.textContent = p1Score;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
+        }
+        p1Display.textContent = p1Score;
     }
 });
 
-p2Button.addEventListener('click', function() {
-    if(!isGameOver){
+p2Button.addEventListener('click', function () {
+    if (!isGameOver) {
         p2Score += 1;
-    if (p2Score === winningScore) {
-        isGameOver = true;
-        p2Display.classList.add('has-text-success');
-        p1Display.classList.add('has-text-danger');
-        p1Button.disabled = true;
-        p2Button.disabled = true;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
 
-    }
-    p2Display.textContent = p2Score;
+        }
+        p2Display.textContent = p2Score;
     }
 });
 
@@ -48,7 +48,7 @@ winningScoreSelect.addEventListener('change', function () {
 resetButton.addEventListener('click', reset)
 
 
-function reset() {  
+function reset() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
@@ -57,6 +57,6 @@ function reset() {
     p1Display.classList.remove('has-text-success', 'has-text-danger');
     p2Display.classList.remove('has-text-success', 'has-text-danger');
     p1Button.disabled = false;
-        p2Button.disabled = false;
+    p2Button.disabled = false;
 
 };
